466f4b14a52b7699cc0c16fb073da0fc823b727b
Gregory Pozo <blacksanin24@gmail.com>
2018-10-31 09:46:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
     let myChart = document.getElementById('myChart').getContext('2d');
 
     // Global Options
